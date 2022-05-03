92780e9c23c3f6c3aa3ff312b71035e2a2e0c0f2
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2022-05-03 12:39:10 +0300

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :pledgers
+  resources :pledgers, except: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/vatfevhsdgffgytfdshfhdfyrdcsfgcg', to: 'pledgers#index'
   root to: 'pledgers#new'
 end
